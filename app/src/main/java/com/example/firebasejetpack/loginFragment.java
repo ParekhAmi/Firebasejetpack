d940c8b40a580ce488a95a5f6297e1e49f95893a
Ami <amiparekh87@gmail.com>
2019-09-11 16:51:05 -0400

    Email&Password Validation

--- app/src/main/java/com/example/firebasejetpack/loginFragment.java
+++ app/src/main/java/com/example/firebasejetpack/loginFragment.java
@@ -4,107 +4,122 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link loginFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link loginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class loginFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class loginFragment extends Fragment implements View.OnClickListener {
+   EditText edt_email,edt_pass;
+   Button btn_log;
+   TextView txt_reg;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+   private FirebaseAuth auth;
+   FirebaseUser user;
 
-    private OnFragmentInteractionListener mListener;
+   public loginFragment(){
 
-    public loginFragment() {
-        // Required empty public constructor
-    }
+   }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment loginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static loginFragment newInstance(String param1, String param2) {
-        loginFragment fragment = new loginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        edt_email = view.findViewById(R.id.edt_email);
+        edt_pass = view.findViewById(R.id.edt_pass);
+        btn_log = view.findViewById(R.id.btn_log);
+        txt_reg = view.findViewById(R.id.txt_lrge);
+
+        btn_log.setOnClickListener(this);
+        txt_reg.setOnClickListener(this);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        auth = FirebaseAuth.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+
+
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+    public void onClick(View view) {
+        int id = view.getId();
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        if(id == R.id.btn_log)
+        {
+            if(TextUtils.isEmpty(edt_email.getText().toString()))
+            {
+                edt_email.setError("Email cannot be blank!");
+                edt_email.requestFocus();
+            }else if(TextUtils.isEmpty(edt_pass.getText().toString()))
+            {
+                edt_pass.setError("Password cannot be blank!");
+                edt_pass.requestFocus();
+            }else
+            {
+                if(edt_pass.getText().toString().length()<6)
+                {
+                    edt_pass.setError("Password should be 6 characters!");
+                    edt_pass.requestFocus();
+                }else
+                {
+                    String email = edt_email.getText().toString();
+                    String pass = edt_pass.getText().toString();
+
+                    loginUser(email,pass);
+                }
+            }
+        }else if(id == R.id.txt_lrge)
+        {
+
+        }
+    }
+    public void loginUser(String email,String pass)
+    {
+        auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    user = auth.getCurrentUser();
+                    Toast.makeText(getActivity().getApplicationContext(),"Login Success",Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    Toast.makeText(getActivity().getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
